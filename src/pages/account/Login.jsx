2d570026a66f677555be412470ba9969b634ec27
supesu <kian.pynk@gmail.com>
2021-04-24 20:23:50 +1200

    rework: login system

--- src/pages/account/Login.jsx
+++ src/pages/account/Login.jsx
@@ -82,11 +82,44 @@ class Login extends Component {
     });
 
     return (
-        <div id="contianer" className="h-screen flex items-center justify-center">
-            <LogoLarge />
-            <div className="flex items-center">
+      <div id="contianer" className="h-screen flex items-center justify-center">
+        <div class="">
+          <LogoLarge className="mx-auto mb-8" />
+          <form onSubmit={this.handleSubmit}>
+            <div class="form__group field mb-4">
+              <input
+                type="email"
+                class="form__field"
+                placeholder="email"
+                name="email"
+                id="email"
+                required
+              />
+              <label for="email" class="form__label">
+                Email address
+              </label>
             </div>
+
+            <div class="form__group field">
+              <input
+                type="password"
+                class="form__field"
+                placeholder="Password"
+                name="Password"
+                id="Password"
+                required
+              />
+              <label for="Password" class="form__label">
+                Password
+              </label>
+            </div>
+
+            <button type="submit" className="flex outline-none focus:ring-4 focus:ring-secondary py-2 px-6 text-sm rounded-lg  text-button bg-primary-700 hover:bg-primary-600 disabled:text-primary-300 font-bold flex items-center justify-center justify-center text-base py-3 mt-5">
+                test
+            </button>
+          </form>
         </div>
+      </div>
     );
   }
 }
